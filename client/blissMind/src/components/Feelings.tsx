0e3e20c81e8d4f03817a02288f1faf9a7e1ecf6b
kamal <kamalthapaliya7@gmail.com>
2024-06-12 15:54:30 +0545

    feelings component edited

--- client/blissMind/src/components/Feelings.tsx
+++ client/blissMind/src/components/Feelings.tsx
@@ -2,7 +2,7 @@ import {RiEmotionLaughLine, RiEmotionNormalLine, RiEmotionSadLine, RiEmotionUnha
 
 const Feelings = () => {
     return (
-        <div className={`my-10 flex flex-col items-center justify-center gap-6`}>
+        <div className={`my-5 py-5 flex flex-col items-center justify-center gap-6 shadow`}>
             <p className={`font-medium text-2xl`}>How are you feeling today?</p>
             <div className={`flex gap-4 text-4xl`}>
                 <div className={`bg-amber-100 p-6 rounded-full shadow-lg cursor-pointer hover:bg-amber-200 transition`}>
